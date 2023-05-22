600205f5722270abf693dc875ab73098ca79c161
Victor Giovanni Beltrán Rodríguez <vgbr.dev@gmail.com>
2023-05-22 17:04:31 -0600

    refactor: add jsdoc comments and new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/**
+ * @file Entry point of the React application.
+ * @module App
+ */
 
-function App() {
-  const [count, setCount] = useState(0)
+// ━━ IMPORT MODULES ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// » IMPORT REACT MODULES
+import React from 'react';
+// » IMPORT HOOKS
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+// » IMPORT COMPONENTS
+import Header from './components/common/Header';
+import Main from './components/common/Main';
 
-export default App
+// ━━ COMPONENT ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+/**
+ * The `App` component, the main component where the entire `react` application
+ * is managed.
+ *
+ * @component
+ * @returns {JSX.Element} The rendered component.
+ */
+const App = () => (
+  <React.Fragment key="App">
+    <Header />
+    <Main />
+  </React.Fragment>
+);
+
+// ━━ EXPORT MODULE ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+export default App;
