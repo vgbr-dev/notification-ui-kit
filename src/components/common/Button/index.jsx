8c513ef9fbb548dc395597c96d2ac3450adfd424
Victor Giovanni Beltrán Rodríguez <vgbr.dev@gmail.com>
2023-05-23 12:32:21 -0600

    refactor: rename component prop

--- src/components/common/Button/index.jsx
+++ src/components/common/Button/index.jsx
@@ -14,14 +14,14 @@ import { PropTypes } from 'prop-types';
  * @param {Object} props - The component properties.
  * @param {React.ReactNode} props.modifier - The modifier class name for the button.
  * @param {boolean} props.text - The text content of the button.
- * @param {Function} props.handleClick - The click event handler for the button.
+ * @param {Function} props.onClick - The click event handler for the button.
  * @returns {JSX.Element} TODO ADD DESCRIPTION.
  */
-const Button = ({ modifier, text, handleClick }) => (
+const Button = ({ modifier, text, onClick }) => (
   <button
     type="button"
     className={`button-container__button button-container__button--${modifier}`}
-    onClick={handleClick}
+    onClick={onClick}
   >
     {text}
   </button>
@@ -30,7 +30,7 @@ const Button = ({ modifier, text, handleClick }) => (
 Button.propTypes = {
   modifier: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
-  handleClick: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 // ━━ EXPORT MODULE ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
