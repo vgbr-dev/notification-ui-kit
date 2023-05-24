f287133485dcb12b560decd54e8f9cb757959493
Victor Giovanni Beltrán Rodríguez <vgbr.dev@gmail.com>
2023-05-24 14:55:06 -0600

    refactor: remove custom hook

--- src/components/common/Toast/index.jsx
+++ src/components/common/Toast/index.jsx
@@ -6,27 +6,100 @@
 // ━━ IMPORT MODULES ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 // » IMPORT REACT MODULES
 import { PropTypes } from 'prop-types';
+import { useEffect, useState } from 'react';
 
-// » IMPORT CUSTOM HOOKS
-import useToast from '../../../hooks/useToast';
+// ━━ TYPE DEFINITIONS ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+/**
+ * Represents the icons used for different toast types.
+ *
+ * @typedef  {object} Icons
+ * @property {string} success     - The icon for success toasts.
+ * @property {string} error       - The icon for error toasts.
+ * @property {string} information - The icon for information toasts.
+ * @property {string} warning     - The icon for warning toasts.
+ */
+
+/**
+ * Represents a toast.
+ *
+ * @typedef  {object}  Toast
+ * @property {string}  id          - The unique ID of the toast.
+ * @property {string}  type        - The type of the toast.
+ * @property {string}  title       - The title of the toast.
+ * @property {string}  description - The description of the toast.
+ * @property {boolean} autoClose   - Determines if the toast should automatically close.
+ */
+
+/**
+ * Function to close a toast.
+ *
+ * @typedef  {(id: number) => void} CloseToast
+ */
+
+// ━━ CONSTANTS ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+/**
+ * Icons for different toast types.
+ *
+ * @type {Icons}
+ */
+const icons = {
+  success: 'check_circle',
+  error: 'error',
+  information: 'info',
+  warning: 'warning',
+};
+
+/**
+ * The delay in milliseconds for automatically closing a toast.
+ *
+ * @constant {number} AUTO_CLOSE_DELAY
+ */
+const AUTO_CLOSE_DELAY = 5000; // 5 seconds
 
 // ━━ COMPONENT ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 /**
  * The `Toast` component.
  *
  * @component
+ * @param {object} props - The Component properties.
+ * @param {Toast} props.toast -The toast properties.
+ * @param {CloseToast} props.closeToast - Function to close a toast.
  * @returns {JSX.Element} The rendered component.
  */
 const Toast = ({ toast, closeToast }) => {
-  const { toastClass, icon, title, description, onClick } = useToast(toast, closeToast);
+  const toastTypeClass = `toast toast--${toast.type}`;
+  const toastAutoCloseClass = toast.autoClose ? ' toast--auto-close' : '';
+  const [toastClass, setToastClass] = useState(`${toastTypeClass} ${toastAutoCloseClass}`);
+
+  /**
+   * Handles the click event on a toast.
+   */
+  const onClick = () => {
+    setToastClass(previous => `${previous} toast--closing`);
+    setTimeout(() => {
+      closeToast(toast.id);
+    }, 200);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (toast.autoClose) {
+        closeToast(toast.id);
+      }
+    }, AUTO_CLOSE_DELAY);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [closeToast, toast.autoClose, toast.id]);
 
   return (
-    <div className={toastClass}>
+    <div id={toast.id} className={toastClass}>
       <div className="toast__content">
-        <div className="toast__icon">{icon}</div>
+        <div className="toast__icon">{icons[toast.type]}</div>
         <div className="toast__text">
-          <p className="toast__title">{title}</p>
-          <p className="toast__description">{description}</p>
+          <p className="toast__title">{toast.title}</p>
+          <p className="toast__description">{toast.description}</p>
         </div>
       </div>
       <div className="toast__control">
