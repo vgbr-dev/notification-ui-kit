53a35ad3f63c1a74b3e07cbfed459eda39bce201
Victor Giovanni Beltrán Rodríguez <vgbr.dev@gmail.com>
2023-05-25 13:08:58 -0600

    refactor: change `useToasts` custom Hook for managing toasts
    The useToasts custom Hook provides functionality to manage toast notifications. It allows adding new toasts and closing existing ones. The hook utilizes React's useState, useEffect, useRef, useMemo, and useCallback hooks for state management and DOM manipulation.
    
    Changes:
    - Upgraded the version of the useToasts module to 1.3.0.
    - Imported additional React modules: useEffect, useRef, useMemo, and useCallback.
    - Imported the Toast component for rendering individual toasts.
    - Improved the type definitions for toast notifications.
    - Implemented a portal element to render toasts outside the main component tree.
    - Created utility functions for managing the portal element.
    - Refactored the closeToast and addToast functions to use useCallback for better performance.
    - Added a useEffect hook to initialize and cleanup the portal element.
    - Updated the return value of the useToasts hook to include the toastsContainer and addToast properties.

--- src/hooks/useToasts.jsx
+++ src/hooks/useToasts.jsx
@@ -1,20 +1,24 @@
 /**
  * @file Manage `useToasts` custom Hook, for managing toasts.
  * @module useToasts
- * @version 1.2.0
+ * @version 1.3.0
  */
 
 // ━━ IMPORT MODULES ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 // » IMPORT REACT MODULES
-import { useState } from 'react';
+import React, { useState, useEffect, useRef, useMemo, useCallback } from 'react';
+import { createPortal } from 'react-dom';
+
+// » IMPORT COMPONENTS
+import Toast from '../components/common/Toast';
 
 // ━━ TYPE DEFINITIONS ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 /**
- * Represents a toast.
+ * Represents the properties of toast notifications.
  *
  * @typedef  {object}  Toast
  * @property {string}  id          - The unique ID of the toast.
- * @property {string}  type        - The type of the toast.
+ * @property {string}  type        - The type of the toast (success, error, information, warning).
  * @property {string}  title       - The title of the toast.
  * @property {string}  description - The description of the toast.
  * @property {boolean} autoClose   - Determines if the toast should automatically close.
@@ -27,24 +31,33 @@ import { useState } from 'react';
  */
 
 /**
- * Function to close a toast.
+ * Function to close a toast notifications.
  *
  * @typedef  {(id: number) => void} closeToast
  */
 
 /**
- * Function to add a new toast.
+ * The options to create toast notifications.
  *
- * @typedef  {(toast: Toast) => void} addToast
+ * @typedef  {object}  AddToastOptions -
+ * @property {string}  type            - The type of the toast (success, error, information, warning).
+ * @property {string}  title           - The title of the toast.
+ * @property {string}  description     - The description of the toast.
+ * @property {boolean} autoClose       - Determines if the toast should automatically close.
+ */
+
+/**
+ * Function to add a new toast notifications.
+ *
+ * @typedef  {(toast: AddToastOptions) => void} addToast
  */
 
 /**
  * Represents the `useToasts` custom hook for managing toasts.
  *
- * @typedef  {object}     ToastsHook
- * @property {Toasts}     toasts     - An array of toasts.
- * @property {closeToast} closeToast - Function to close a toast.
- * @property {addToast}   addToast   - Function to add a new toast.
+ * @typedef  {object}           ToastsHook
+ * @property {HTMLElement|null} toastsContainer - The DOM container element for the toasts.
+ * @property {addToast}         addToast        - Function to add a new toast notifications.
  */
 
 // ━━ FUNCTIONS ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -55,48 +68,121 @@ import { useState } from 'react';
  */
 const createToastId = () => `${Date.now().toString(36)}-${Math.random().toString(36).substr(2)}`;
 
+/**
+ * Retrieves or creates a portal element based on the provided ID and className.
+ *
+ * @param {string} id - The ID of the portal element.
+ * @param {string} className - The CSS class name to be applied to the portal element.
+ * @returns {HTMLElement} - The portal element.
+ */
+const getPortalElement = (id, className) => {
+  const existingPortal = document.getElementById(id);
+
+  if (existingPortal) {
+    return existingPortal;
+  }
+
+  const portalElement = document.createElement('div');
+  portalElement.id = id;
+  portalElement.classList.add(className);
+
+  return portalElement;
+};
+
+/**
+ * Appends the provided element to the document body if it doesn't already exist.
+ *
+ * @param {HTMLElement} element - The element to be appended to the body.
+ */
+const appendChildToBody = element => {
+  if (!document.body.contains(element)) {
+    document.body.appendChild(element);
+  }
+};
+
+/**
+ * Removes the provided element from the document body.
+ *
+ * @param {HTMLElement} element - The element to be removed from the body.
+ */
+const removeChildFromBody = element => {
+  if (element && element.parentNode === document.body) {
+    document.body.removeChild(element);
+  }
+};
+
 // ━━ CUSTOM HOOK ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 /**
  * The `useToasts` custom hook for managing toasts.
  *
- * @returns {ToastsHook} An object with `toasts` state and functions for manage it.
+ * @returns {ToastsHook} An object with `toasts` state and functionsfor manage it.
+ * @example const { toastsContainer, addToast } = useToasts();
  */
 const useToasts = () => {
   const [toasts, setToasts] = useState([]);
+  const portalRef = useRef(null);
 
   /**
    * Closes a toast with the specified ID.
    *
    * @param {number} id - The ID of the toast to close.
    */
-  const closeToast = id => {
+  const closeToast = useCallback(id => {
     setToasts(previous => previous.filter(toast => toast.id !== id));
-  };
+  }, []);
 
   /**
    * Adds a new toast.
    *
-   * @param {object} options - The options for the new toast.
-   * @param {string} options.type - The type of the toast.
-   * @param {string} options.title - The title of the toast.
-   * @param {string} options.description - The description of the toast.
-   * @param {boolean} options.autoClose - Determines if the toast should automatically close.
+   * @param {AddToastOptions} options - The options for the new toast.
    */
-  const addToast = ({ type, title, description, autoClose }) => {
-    const toast = {
-      id: createToastId(),
-      type,
-      title,
-      description,
-      autoClose,
+  const addToast = useCallback(
+    ({ title, description, type, autoClose }) => {
+      const toast = {
+        id: createToastId(),
+        type,
+        title,
+        description,
+        autoClose,
+      };
+      setToasts(previous => [...previous, toast]);
+    },
+    [setToasts],
+  );
+
+  useEffect(() => {
+    portalRef.current = getPortalElement('toasts-container', 'toasts');
+    appendChildToBody(portalRef.current);
+    return () => {
+      if (portalRef.current) {
+        removeChildFromBody(portalRef.current);
+        portalRef.current = null;
+      }
     };
-    setToasts([...toasts, toast]);
-  };
+  }, []);
+
+  /**
+   * Returns the container element for the toasts.
+   *
+   * @returns {ReactNode|null} - The container element for the toasts.
+   */
+  const toastsContainer = useMemo(() => {
+    const condition = portalRef.current !== null;
+    return condition
+      ? createPortal(
+          <React.Fragment key="toast">
+            {toasts.map(toast => (
+              <Toast key={toast.id} toast={toast} closeToast={closeToast} />
+            ))}
+          </React.Fragment>,
+          portalRef.current,
+        )
+      : null;
+  }, [portalRef, closeToast, toasts]);
 
   return {
-    toasts,
+    toastsContainer,
     addToast,
-    closeToast,
   };
 };
 
