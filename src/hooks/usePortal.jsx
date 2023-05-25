c0f3e9e05cfff4f17bdb1e22b13ea8f3f0cda774
Victor Giovanni Beltrán Rodríguez <vgbr.dev@gmail.com>
2023-05-25 12:43:31 -0600

    feat(usePortal): update usePortal hook to version 1.1.0
    This update introduces the following changes to the usePortal hook:
    - Refactored code for improved readability and maintainability.
    - Updated dependencies to use the latest versions of React, ReactDOM, and PropTypes.
    - Improved the retrieval and creation of portal elements based on provided ID and className.
    - Added type definitions for the portal-related functions and options.
    - Renamed the openPortal, closePortal, and togglePortal functions for clarity.
    - Updated the example code in the hook's documentation.
    
    BREAKING CHANGES:
    - The openPortal, closePortal, and togglePortal functions have been renamed for clarity. Please update your code accordingly.

--- src/hooks/usePortal.jsx
+++ src/hooks/usePortal.jsx
@@ -1,15 +1,27 @@
 /**
- * @file Manage `usePortal` custom React Hook.
+ * @file Manage `usePortal` custom React Hook for creating a portal.
+ *
+ * This hook provides functions and state for creating a portal in React.
+ * It allows rendering components outside the normal React component hierarchy
+ * and provides control over the visibility of the portal.
+ *
  * @module usePortal
- * @version 1.0.0
+ * @version 1.1.0
  */
 
 // ━━ IMPORT MODULES ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 // » IMPORT REACT MODULES
-import { useEffect, useRef, useState } from 'react';
-import ReactDOM from 'react-dom';
+import { useState, useEffect, useRef } from 'react';
+import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
 
 // ━━ TYPE DEFINITIONS ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+/**
+ * @typedef  {Object} PortalOptions - The options for the portal.
+ * @property {string} id            - The ID of the portal element. Defaults to `portal` if not provided.
+ * @property {string} className     - The CSS class name to be applied to the portal element.
+ */
+
 /**
  * The function to open the Portal component.
  *
@@ -23,76 +35,133 @@ import ReactDOM from 'react-dom';
  */
 
 /**
- * The function to toggle Portal component visibility.
+ * The function to toggle the state of the Portal component between `open` and `closed`.
  *
  * @typedef {() => void} TogglePortal
  */
 
 /**
- * The Portal object.
+ * An object containing portal-related functions and state.
  *
  * @typedef  {object}          PortalHook
  * @property {React.Component} Portal       - The React component for rendering the Portal component.
  * @property {OpenModal}       openPortal   - The function to open the Portal component.
  * @property {CloseModal}      closePortal  - The function to close the Portal component.
- * @property {ToggleModal}     togglePortal - The function to toggle Portal component visibility.
+ * @property {ToggleModal}     togglePortal - The function to toggle the state of the Portal component between `open` and `closed`.
  */
 
 // ━━ FUNCTIONS ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 /**
- * Append the element to the body.
+ * Retrieves or creates a portal element based on the provided ID and className.
  *
- * @param {HTMLElement} element - The element to be appended.
+ * @param {string} id - The ID of the portal element.
+ * @param {string} className - The CSS class name to be applied to the portal element.
+ * @returns {HTMLElement} - The portal element.
+ */
+const getPortalElement = (id, className) => {
+  const existingPortal = document.getElementById(id);
+
+  if (existingPortal) {
+    return existingPortal;
+  }
+
+  const portalElement = document.createElement('div');
+  portalElement.id = id;
+  portalElement.classList.add(className);
+
+  return portalElement;
+};
+
+/**
+ * Appends the provided element to the document body if it doesn't already exist.
+ *
+ * @param {HTMLElement} element - The element to be appended to the body.
  */
 const appendChildToBody = element => {
-  document.body.appendChild(element);
+  if (!document.body.contains(element)) {
+    document.body.appendChild(element);
+  }
+};
+
+/**
+ * Removes the provided element from the document body.
+ *
+ * @param {HTMLElement} element - The element to be removed from the body.
+ */
+const removeChildFromBody = element => {
+  if (element && element.parentNode === document.body) {
+    document.body.removeChild(element);
+  }
 };
 
 // ━━ CUSTOM HOOK ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 /**
- * Custom React hook for managing a Pop-up modal.
+ * Retrieves or creates a portal element based on the provided ID and className.
  *
- * @param {PortalOptions} options - The options for the Pop-up modal.
- * @returns {PortalHook} - The Pop-up modal object.
- * @example const { PopUp, togglePopUp, closePopUp } = usePortal(options);
+ * @param {PortalOptions} options - The options for the hook.
+ * @returns {PortalHook} - An object containing portal-related functions and state.
+ * @example const { Portal, openPortal, closePortal, togglePortal, } = usePortal(options);
  */
 const usePortal = ({ id, className }) => {
-  const [visibility, setVisibility] = useState(true);
-  const rootElement = document.createElement('div');
-  const containerRef = useRef(rootElement);
-  containerRef.current.setAttribute('id', id);
-  containerRef.current.setAttribute('class', className);
-
-  useEffect(() => {
-    const element = containerRef.current;
-    if (visibility) {
-      appendChildToBody(element);
-    }
-    return () => {
-      element.remove();
-    };
-  }, [visibility]);
+  const [visibility, setVisibility] = useState(false);
+  const portalRef = useRef(null);
 
   /**
    * Open the Portal component.
    */
-  const openPortal = () => setVisibility(true);
+  const openPortal = () => {
+    setVisibility(true);
+  };
 
   /**
-   * Close the Portal component.
+   * The `closePortal()` function to toggle the state of the object
+   * between `open` and `closed`.
+   *
    */
-  const closePortal = () => setVisibility(false);
+  const closePortal = () => {
+    setVisibility(false);
+  };
 
   /**
-   * Toggle the Portal component visibility.
+   * The `togglePortal()` function to toggle the state of the Portal component
+   * between `open` and `closed`.
+   *
    */
-  const togglePortal = () => setVisibility(!visibility);
+  const togglePortal = () => {
+    setVisibility(prevVisibility => !prevVisibility);
+  };
+
+  useEffect(() => {
+    portalRef.current = getPortalElement(id, className);
+    if (visibility) {
+      appendChildToBody(portalRef.current);
+    }
+    return () => {
+      if (portalRef.current) {
+        removeChildFromBody(portalRef.current);
+        portalRef.current = null;
+      }
+    };
+  }, [id, className, visibility]);
+
+  /**
+   * Renders the portal component.
+   *
+   * @param {object} props - The props for the portal component.
+   * @param {ReactNode} props.children - The children to be rendered within the portal.
+   * @returns {ReactNode|null} - The portal component or null if visibility is false or portalRef is null.
+   */
+  const Portal = ({ children }) => {
+    const condition = visibility && portalRef.current;
+    return condition ? createPortal(children, portalRef.current) : null;
+  };
+
+  Portal.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
 
   return {
-    Portal: ({ children }) => {
-      const condition = visibility && containerRef.current !== null;
-      return condition ? ReactDOM.createPortal(children, containerRef.current) : null;
-    },
+    Portal,
     openPortal,
     closePortal,
     togglePortal,
