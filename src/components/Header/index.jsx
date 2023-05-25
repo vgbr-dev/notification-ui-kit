3b0a8faff0faf664f2a1f7202ce0f645e4c8fecc
Victor Giovanni Beltrán Rodríguez <vgbr.dev@gmail.com>
2023-05-25 12:12:08 -0600

    style: change html element

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -13,9 +13,9 @@
 const Header = () => (
   <header id="header-wrapper" className="app-header">
     <h1 className="app-header__title">Toats</h1>
-    <h2 className="app-header__subtitle">
+    <h1 className="app-header__subtitle">
       & <span className="app-header__subtitle--accent">Modals</span>
-    </h2>
+    </h1>
     <h4 className="app-header__description">
       A code repository that stores components for toasts, popups, and modals for potential future
       use in web development projects.
